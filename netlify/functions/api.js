e2e4d7944f31fc95ff6fb51171edba01ca475e6b
midbar40 <jklhdd@naver.com>
2024-09-17 16:00:20 +0900

    test_0917_05

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -35,18 +35,18 @@ api.use(logger('tiny')) // logger 설정
 api.use(cookieParser())
 
 // 라우터 설정
-api.use('/api/bible', biblesRouter)
-// api.use('/api/users', usersRouter)
-// api.use('/api/prayBucketlist', prayBucketlistRouter)
-// api.use('/api/grace', graceRouter)
-// api.use('/api/prayDiary', prayDiaryRouter)
-// api.use('/api/pickPosts', pickPostRouter)
-// api.use('/api/bibleParagraphs', bibleParagraphsRouter)
-// api.use('/api/sermon', sermonRouter)
-// api.use('/api/otp', otpRouter)
+api.use('/bible', biblesRouter)
+api.use('/users', usersRouter)
+api.use('/prayBucketlist', prayBucketlistRouter)
+api.use('/grace', graceRouter)
+api.use('/prayDiary', prayDiaryRouter)
+api.use('/pickPosts', pickPostRouter)
+api.use('/bibleParagraphs', bibleParagraphsRouter)
+api.use('/sermon', sermonRouter)
+api.use('/otp', otpRouter)
 
 
-// fallback handler
+// // fallback handler
 // api.use((req, res, next) => { // 사용자가 요청한 페이지가 없는 경우 에러처리
 //         res.status(404).send('페이지를 찾을 수 없습니다, 여기는 들어온다')
 //     })
